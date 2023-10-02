4da4ee231a745c7cfd2d798f49834f634c0f3aec
Akshay <jainakshay058@gmail.com>
2023-10-02 14:36:15 +0530

    code

--- src/sheetClient.js
+++ src/sheetClient.js
@@ -2,7 +2,7 @@ import { google } from "googleapis";
 import dotenv from "dotenv";
 
 // Importing the credentials (client_email and private_key) from the secrets.json file
-import key from "../secrets.json" assert { type: "json" };
+// import key from "../secrets.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -10,9 +10,12 @@ dotenv.config();
 export const SHEET_ID = "1Ms_HnCkGgZ4lOHHpfCChGZ1HuUuPQwufydfd7wur5zc";
 
 // Created a new JWT (JSON Web Token) client using the provided credentials
-const client = new google.auth.JWT(key.client_email, null, key.private_key, [
-  "https://www.googleapis.com/auth/spreadsheets",
-]);
+const client = new google.auth.JWT(
+  process.env.client_email,
+  null,
+  process.env.private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 
 // Creating a Google Sheets API client using the JWT client
 const sheets = google.sheets({ version: "v4", auth: client });
